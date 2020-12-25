a0f9cb051a8c09c2719af8535a847f7b0e2905ad
rende11 <artamonovn@gmail.com>
2020-12-25 19:05:11 +0400

    ui continue

--- ui/src/app/pages/patient/view.cljs
+++ ui/src/app/pages/patient/view.cljs
@@ -1,7 +1,16 @@
 (ns app.pages.patient.view
   (:require [zframes.pages :as pages]
             [re-frame.core :as rf]
+            [clojure.string :as str]
             [app.pages.patient.model :as model]))
+
+(defn get-age [bd]
+  (let [bd-date (js/Date. bd)
+        diff (- (.now js/Date.) (.getTime bd-date))
+        age (js/Date. diff)
+        full-year (.getUTCFullYear age)]
+    (js/Math.abs (- full-year 1970))))
+
 
 (pages/reg-subs-page
  model/view-page
@@ -11,13 +20,38 @@
      [:div.bd.bgc-white
       [:div.pX-30.pY-20.peers.ai-c
        [:div.peer.mR-20
-           [:img.bdrs-50p.w-3r.h-3r {:src (str "https://i.pravatar.cc/150?u=" (:id p))}]]
-       (pr-str (:name p))]
-      ]
+        [:img.bdrs-50p.w-3r.h-3r {:src (str "https://i.pravatar.cc/150?u=" (:id p))}]]
+       [:div.peer.peer-greed.pL-20.flex
+        [:h5.c-grey-900.mB-5
+         (str/replace (str (get-in p [:name 0 :given 0]) " " (get-in p [:name 0 :family])) #"\d+" "")]
+        [:div
+         [:span.mR-20 {:style {:color "#72777a"}}
+          [:i.ti-user.mR-5]
+          (str
+           (first (str/capitalize (or (:gender p) ""))) " "
+           (get-age (:birthDate p)) "y/o")]
 
+         (when-let [a (:address p)]
+           (let [{:keys [city line state postalCode]} (first a)]
+             [:span.mR-20 {:style {:color "#72777a"}}
+              [:i.ti-home.mR-5]
+              (str (first line) " " city " " state "  " postalCode)]))
 
+         (when-let [t (:telecom p)]
+           [:span.mR-20 {:style {:color "#72777a"}}
+            [:i.ti-mobile.mR-5]
+            (:value (first t))])]
 
-     ]
+        (let [i (:identifier p)
+               ss (:value (first (filter #(= "SS" (-> % :type :coding first :code)) i)))
+              mrn (:value (first (filter #(= "MR" (-> % :type :coding first :code)) i)))]
+          [:div
+           [:span.mR-5 {:style {:font-size "16px"
+                                :font-weight "300"}} "Security Number"]
+           [:span.mR-20 ss]
+           [:span.mR-5 {:style {:font-size "16px"
+                                :font-weight "300"}} "MRN"]
+           [:span.mR-20 mrn]])]]]]
 
     [:div.col-md-12.mt-3
      [:div.bd.bgc-white
